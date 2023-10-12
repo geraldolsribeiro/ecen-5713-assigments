f88b612283c62a02826795d1eb04d237ac0dc919
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2023-10-12 23:59:32 +0400

    assingment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,16 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} clean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    # Solution for "'multiple definition of yylloc' error #4
+    # https://github.com/BPI-SINOVOIP/BPI-M4-bsp/issues/4
+    make -j$(nproc --ignore 1) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all || \
+      sed -i "s/^YYLTYPE yylloc;/extern &/" ${OUTDIR}/linux-stable/scripts/dtc/dtc-lexer.lex.c
+    make -j$(nproc --ignore 1) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j$(nproc --ignore 1) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -49,6 +59,11 @@ fi
 
 # TODO: Create necessary base directories
 
+for d in bin dev etc home lib lib64 proc sbin sys tmp var usr/bin usr/lib usr/sbin var/log
+do
+  mkdir -p "${OUTDIR}/rootfs/$d"
+done
+
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
@@ -56,25 +71,56 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
 
+make -j$(nproc --ignore=1) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd "${OUTDIR}/rootfs"
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 
+SYSROOT=$( ${CROSS_COMPILE}gcc -print-sysroot)
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${SYSROOT}/lib64/libm.so.6           ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libresolv.so.2      ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libc.so.6           ${OUTDIR}/rootfs/lib64
+
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 
+mkdir -p ${OUTDIR}/rootfs/home/conf
+for i in writer finder.sh conf/username.txt conf/assignment.txt finder-test.sh autorun-qemu.sh
+do
+  cp $i ${OUTDIR}/rootfs/home/$i
+done
+
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
+cp ${OUTDIR}/rootfs/sbin/init ${OUTDIR}/rootfs/init
+
 # TODO: Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+rm -f ../initramfs.cpio.gz
+gzip ../initramfs.cpio
