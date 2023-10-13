517b1cbcea13a2e630090dbe5b946a8b3e6521a4
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2023-10-13 23:43:39 +0400

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,10 +10,31 @@
 
 void* threadfunc(void* thread_param)
 {
-
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int rc = 0;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    DEBUG_LOG( "Waiting for %dms", thread_func_args->wait_to_obtain_ms );
+    thread_func_args->thread_complete_success = false;
+    usleep(1000 * thread_func_args->wait_to_obtain_ms);
+    DEBUG_LOG( "LOCK" );
+    rc = pthread_mutex_lock(thread_func_args->mutex);
+    DEBUG_LOG( "rc = %d", rc );
+    if( rc != 0 ) {
+      ERROR_LOG("pthread_mutex_lock failed with %d", rc );
+    } else {
+      DEBUG_LOG( "Thread body" );
+      usleep(1000 * thread_func_args->wait_to_release_ms);
+      DEBUG_LOG( "UNLOCK" );
+      rc = pthread_mutex_unlock(thread_func_args->mutex);
+      DEBUG_LOG( "rc = %d", rc );
+      if( rc != 0 ) {
+        ERROR_LOG("pthread_mutex_unlock failed with %d", rc );
+      } else {
+        thread_func_args->thread_complete_success = true;
+      }
+    }
+
     return thread_param;
 }
 
@@ -28,6 +49,20 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+  int rc;
+  struct thread_data * thread_param = malloc(sizeof(struct thread_data));
+  thread_param->thread_complete_success = false;
+  thread_param->mutex = mutex;
+  thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+  thread_param->wait_to_release_ms = wait_to_release_ms;
+
+  rc = pthread_create(thread, NULL, threadfunc, thread_param );
+  if( rc == 0 ) {
+    return true;
+  }
+  ERROR_LOG("pthread_create failed with %d", rc );
+  free(thread_param);
+  return false;
 }
 
