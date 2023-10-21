5e9c7257aa49e826403e101f3eb8afd8379792ba
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2023-10-21 10:17:05 +0400

    Write output to /tmp/assignment4-result.txt

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -60,6 +60,8 @@ done
 
 OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
+
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
