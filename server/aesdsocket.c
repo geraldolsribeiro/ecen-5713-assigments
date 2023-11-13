ba34873c011aeb4685c169c3cc2258f1259814de
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2023-11-13 11:53:03 +0400

    Update aesdsocket.c
    Restore second fork

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -197,24 +197,24 @@ int main(int argc, char **argv) {
       exit(EXIT_SUCCESS);
     }
 
-    /* if (setsid() < 0) { */
-    /*   exit(EXIT_FAILURE); */
-    /* } */
-    /*  */
-    /* signal(SIGCHLD, SIG_IGN); */
-    /* signal(SIGHUP, SIG_IGN); */
-    /*  */
-    /* pid = fork(); */
-    /* if (pid < 0) { */
-    /*   perror("Fail to fork (second time)"); */
-    /*   exit(EXIT_FAILURE); */
-    /* } */
-    /* if (pid > 0) { */
-    /*   // parent process */
-    /*   exit(EXIT_SUCCESS); */
-    /* } */
-    /* umask(0); */
-    /* chdir("/"); */
+    if (setsid() < 0) {
+      exit(EXIT_FAILURE);
+    }
+    
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+    
+    pid = fork();
+    if (pid < 0) {
+      perror("Fail to fork (second time)");
+      exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+      // parent process
+      exit(EXIT_SUCCESS);
+    }
+    umask(0);
+    chdir("/");
     /* for (int x = sysconf(_SC_OPEN_MAX); x >= 0; x--) { */
     /*   close(x); */
     /* } */
